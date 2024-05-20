4aefab7aad59bb7a6d4bc4a70bd77526d3720953
richy163 <ritchycal@gmail.com>
2024-05-20 11:46:58 -0400

    cleaning up data and changing column names

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+
+df = pd.read_csv('bestsellers.csv')
+df.drop_duplicates(inplace=True)
+df.rename(columns={"Name": "Title", "Year": "Publication", "User Rating": "Rating"}, inplace=True)
+
