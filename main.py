31f5a41acf2eeed808e688a12dc53141fab1c279
richy163 <ritchycal@gmail.com>
2024-05-20 13:54:12 -0400

    added filter for fiction books

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ df = pd.read_csv('bestsellers.csv')
 df.drop_duplicates(inplace=True)
 df.rename(columns={"Name": "Title", "Year": "Publication", "User Rating": "Rating"}, inplace=True)
 df["Price"] = df["Price"].astype(float)
+options = ['Fiction']
+fiction = df[df['Genre'].isin(options)]
+print(fiction)
