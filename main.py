77d6f8a9ccbc490a80657117beb92e58cf493319
richy163 <ritchycal@gmail.com>
2024-05-20 11:48:17 -0400

    changed price to float to easly deal with data

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import pandas as pd
 df = pd.read_csv('bestsellers.csv')
 df.drop_duplicates(inplace=True)
 df.rename(columns={"Name": "Title", "Year": "Publication", "User Rating": "Rating"}, inplace=True)
-
+df["Price"] = df["Price"].astype(float)
